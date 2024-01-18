d5abce67b755d6c98122da1f027b078c565148d8
riteshedureka1 <ritesh_niec2@yahoo.co.in>
2024-01-19 02:03:06 +0530

    INITIAL CODE SETUP COMMIT 2

--- src/main/java/TestingJava.java
+++ src/main/java/TestingJava.java
@@ -1,6 +1,6 @@
 
 public class TestingJava {
-
+//added the comment for second commit test
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("RITESHKESARI");
